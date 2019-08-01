66bf06210cc86d8c5a36445a28389863fb0a7183
Tsunamins <reillyamr@gmail.com>
2019-07-31 21:19:36 -0400

    Done.

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,6 +1,7 @@
 class StatsController < ApplicationController
 
   def index
+    #lab notes this, not sure if it means change it to blank or leave it
     @post_count = Post.count
     @authors_count = Author.count
     @last_post = Post.last
