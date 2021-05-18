9825097e4660f7fd04606b0121a1f97dd190d75b
torpedo-t <55634516+torpedo-t@users.noreply.github.com>
2021-05-18 11:16:07 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,22 @@ Rails.application.routes.draw do
 
   resources :posts, only: %i[index show new create edit update]
 
-  get '/admin/stats', to: 'stats#index'
+  namespace :admin do
+    resources :stats, only: [:index]
+  end
+# Rails gives us a namespace method as a shortcut. So, that we don't have to type out our scope explicitly as well as our module.
+# If you switch from scope to namespace the URL helpers will change. Run rails routes with one active and then run one with the other active and see how the routes change.
+  # scope '/admin', module: 'admin' do
+  #   resources :stats, only: [:index]
+  # end
+
+  # get '/admin/stats', to: 'stats#index'
+  # get '/admin/authors/new', to: 'authors#new'
+  # get '/admin/authors/delete', to: 'authors#delete'
+  # get '/admin/authors/create', to: 'authors#create'
+  # get '/admin/comments/moderate', to: 'comments#moderate'
 
   root 'posts#index'
+
+
 end
